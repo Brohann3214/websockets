0b27f5cc9c6a5b768c55cfe0f88e9663c9d8dbfa
Brohann <129230434+Brohann3214@users.noreply.github.com>
2024-09-13 18:49:50 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 
 game.consoleOverlay.setVisible(true)
 control.runInParallel(function () {
-    console.log(`connecting to ws://make-com.ddns.net:443`)
+    console.log(`connecting to ws://127.0.0.1:443`)
 
     // string tests
-    const ws = new WebSocket("ws://make-com.ddns.net:443")
+    const ws = new WebSocket("ws://127.0.0.1:443")
     ws.onerror = () => console.log("error")
     ws.onmessage = (msg) => {
         const data = msg.data;
