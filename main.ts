f815431c5fd62bdbea30fbfaf901d056aecd9e0d
Brohann <129230434+Brohann3214@users.noreply.github.com>
2024-09-13 18:18:53 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+
+game.consoleOverlay.setVisible(true)
+control.runInParallel(function () {
+    console.log(`connecting to wss://make-com.ddns.net:443`)
+
+    // string tests
+    const ws = new WebSocket("wss://make-com.ddns.net:443")
+    ws.onerror = () => console.log("error")
+    ws.onmessage = (msg) => {
+        const data = msg.data;
+        console.log(`[Recieved] ${data}`)
+    }
+    ws.onopen = () => {
+        const msg = "connected. you Win";
+        ws.send(msg);
+        console.log(`[Sent] ${msg}`);
+    }
+})
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 320
+    export const ARCADE_SCREEN_HEIGHT = 240
+}
+scene.setBackgroundColor(11)
