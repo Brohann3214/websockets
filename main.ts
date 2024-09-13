2c7e0e5f553d7ccb7835a6f45b54b728cc2be68e
Brohann <129230434+Brohann3214@users.noreply.github.com>
2024-09-13 18:22:38 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ control.runInParallel(function () {
         console.log(`[Recieved] ${data}`)
     }
     ws.onopen = () => {
-        const msg = "connected. you Win";
+        const msg = "no ip is working";
         ws.send(msg);
         console.log(`[Sent] ${msg}`);
     }
